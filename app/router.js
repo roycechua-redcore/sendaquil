64d4b49ff1435482d435bc757a8a4be57eeddddf
Martha Dansyle Marbella <marthadansylemarbella@gmail.com>
2021-07-26 22:23:54 +0800

    First commit

--- app/router.js
+++ app/router.js
@@ -41,7 +41,7 @@ class Router extends React.Component {
 		switch (route) {
 			default:
 				return (
-					null
+					<div>hello</div>
 				);
 		}
 	}
---
+++ app/test/testScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class testScreen extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
